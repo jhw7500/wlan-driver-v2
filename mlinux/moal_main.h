08dd86b924bf188d1a9f231e1a943a3efb30b816
AMIT CHOUDHARY <amit.kumar_13@nxp.com>
2025-08-25 16:50:10 +0530

    Issue: WCSWREL-733 i.MX Linux: MM6X18540.p7: nanotls related files been wrong added which may cause license confilct
    Removed the default config got SECURE_HOST which leads to
    addition for nanotls dependency..
    
    Signed-off-by: AMIT CHOUDHARY <amit.kumar_13@nxp.com>

--- mlinux/moal_main.h
+++ mlinux/moal_main.h
@@ -2888,7 +2888,6 @@ typedef struct _moal_mod_para {
 	int tpe_ie_ignore;
 	/* make_before_break during roam */
 	int make_before_break;
-	int secure_host;
 } moal_mod_para;
 
 void woal_tp_acnt_timer_func(void *context);
@@ -3482,8 +3481,6 @@ struct _moal_handle {
 	/* fw cap and cap_ext */
 	mlan_hw_info hw_info;
 #endif /* UAP_SUPPORT */
-
-	void *secure;
 
 #ifdef DUMP_TO_PROC
 #if defined(PCIE)
