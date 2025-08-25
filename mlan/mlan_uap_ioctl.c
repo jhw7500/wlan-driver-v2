9ef3d49b288df3c58125cee160abf64f665422f4
Sherry Sun <sherry.sun@nxp.com>
2025-08-25 11:26:19 +0800

    mxm_wifiex: fix build warnings in MM6X18540.p7 release
    Fix the following build warnings in MM6X18540.p7 release.
    
    mwifiex/mlan/mlan_uap_ioctl.c:2037:13: warning: no previous prototype for ‘wlan_uap_agcs_cfg’ [-Wmissing-prototypes]
     2037 | mlan_status wlan_uap_agcs_cfg(pmlan_adapter pmadapter,
          |             ^~~~~~~~~~~~~~~~~
    mwifiex/mlinux/moal_main.c:4375:13: warning: no previous prototype for ‘woal_get_ind_rst_cfg’ [-Wmissing-prototypes]
     4415 | mlan_status woal_get_ind_rst_cfg(moal_handle *handle, t_u8 wait_option)
          |             ^~~~~~~~~~~~~~~~~~~~
    mwifiex/mlinux/moal_main.c:13295:6: warning: no previous prototype for ‘woal_survey_dump_reset’ [-Wmissing-prototypes]
    13335 | void woal_survey_dump_reset(moal_private *priv)
          |      ^~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>

--- mlan/mlan_uap_ioctl.c
+++ mlan/mlan_uap_ioctl.c
@@ -2113,7 +2113,7 @@ static mlan_status wlan_uap_chan_switch_cnt_cfg(pmlan_adapter pmadapter,
  *
  *  @return             MLAN_STATUS_SUCCESS or MLAN_STATUS_FAILURE
  */
-mlan_status wlan_uap_agcs_cfg(pmlan_adapter pmadapter,
+static mlan_status wlan_uap_agcs_cfg(pmlan_adapter pmadapter,
 			      pmlan_ioctl_req pioctl_req)
 {
 	mlan_private *pmpriv = pmadapter->priv[pioctl_req->bss_index];
